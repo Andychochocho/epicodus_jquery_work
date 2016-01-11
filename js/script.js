c0ac2922dff322df9aa9e9d9a663713c35688207
Andrew Cho and Tim Durward <cho-andrew@hotmail.com>
2016-01-11 15:33:49 -0800

    add toggles to images

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
-$("h1").click(function(){
-  alert("This is your reminder to get a burrito today.");
+// $("h1").click(function(){
+//   alert("This is your reminder to get a burrito today.");
+// });
+//
+//
+// $("#soft_drink").click(function(){
+//   $("#soft_drink").hide("fast", function(){
+//   });
+// });
+//
+// $("#burrito").click(function(){
+//   $("#burrito").hide("fast", function(){
+//   });
+// });
+//
+// $("h2").click(function(){
+//   $("img").show("slow", function(){
+//   });
+// });
+
+
+$('#burrito-show').click(function(){
+  $('#burrito').toggle();
 });
 
-
-$("img").click(function(){
-  $("img").hide("fast", function(){
-  });
-});
-
-$("h2").click(function(){
-  $("img").show("slow", function(){
-  });
+$('#soda-show').click(function(){
+  $('#soft_drink').toggle();
 });
